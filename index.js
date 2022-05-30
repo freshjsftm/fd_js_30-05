4972feee0224e9ed4c6ea537017575a436cdea70
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-30 17:12:35 +0300

    Example declaration

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
-20
-'qwe'
-true
-alert(20)
+// console.log("word");
+// console.log(123);
+// console.log(1n);
+// console.log(true);
+// console.log(undefined);
+// console.log(null);
+
+/*
+начинается только: с буквы  или _  или $
+не должно быть пробелов
+регистрозависимое
+camelCase
+отвечает на вопрос что в ней
+*/
+
+
+let userAge2 = 3; 
+userAge2 = 5;      
+const userAge3 = 12n;
+userAge2 = 35; 
+console.log(userAge2);
+// userAge3 = 12;
+
+{
+  let userAge2 = 456;
+  console.log('in block',userAge2);
+}
+
+console.log('under block',userAge2);
+
+
+let userName = null;
+console.log(userName);
+userName = 'Elon';
+console.log(userName);
