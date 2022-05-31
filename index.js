67aea8c9964fb6846f444082057d4c5f5a82e7b6
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-31 15:57:42 +0300

    Add typeof

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 let number = 7;
-
 //number = number * 10;
 number *= 10;
-
 //number = number -2;
 number -= 2;
 
 let string = '68';
+//string /= number;
+//console.log(string); 
 
-string /= number;
+console.log(typeof string);
+console.log(typeof number);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof 'null');
 
-console.log(string); //
+
