cfb9deb2334b2199fbdd431dc38206c30d754f7d
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-31 17:14:05 +0300

    Add logic operators

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
-let number = 7;
-//number = number * 10;
-number *= 10;
-//number = number -2;
-number -= 2;
+/*
+false
+null
+undefined
+'' "" ``
+0 +0 -0
+NaN
+*/
 
-let string = '68';
-//string /= number;
-//console.log(string); 
+//console.log(!NaN);
+// const number1 = 12;
+// const number2 = 15;
+//console.log(number1>13<number2); //ТАК ПИСАТЬ НЕЛЬЗЯ
+// console.log('cat'<'Dog');
 
-console.log(typeof string);
-console.log(typeof number);
-console.log(typeof true);
-console.log(typeof undefined);
-console.log(typeof null);
-console.log(typeof 'null');
+//  ИЛИ логическое сложение - возвращает ПЕРВУЮ правду либо ПОСЛЕДНЮЮ ложь
+//console.log('qwerty'||12||null||false); //true
+// console.log(true||false);//true
+// console.log(false||true);//true
+// console.log(0||null||8);//false
+
+// //  И логическое умножение - возвращает ПЕРВУЮ ложь либо ПОСЛЕДНЮЮ правду
+// console.log(true&&true); //true
+// console.log(true&&false);//false
+// console.log(false&&true);//false
+// console.log(false&&false);//false
+
+
+
+//console.log(number1,number2);
+
+
+// const number1 = 0/0;
+// const number2 = 0/0;
+// console.log(number1 === number2) ;//NaN - unique!!!!
+
 
 
