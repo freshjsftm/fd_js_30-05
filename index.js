323e368a1159c23f82e8bb144b38de6475254346
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-01 15:15:13 +0300

    Add return

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 
-const getSumTwoNumber = function(){
-  const userInputNum1 = prompt('Enter first number');
-  const userInputNum2 = prompt('Enter second number');
+// const getSumTwoNumber = function(){
+   const userInputNum1 = prompt('Enter first number');
+   const userInputNum2 = prompt('Enter second number');
 
   const checkUserInput1 = userInputNum1===null || userInputNum1==='' || isNaN(Number(userInputNum1));
   const checkUserInput2 = userInputNum2===null || userInputNum2==='' || isNaN(Number(userInputNum2));
 
-  if(checkUserInput1 || checkUserInput2){
-    console.log('error')
-  }else{
-    console.log(Number(userInputNum1)+Number(userInputNum2))
-  }
+//   if(checkUserInput1 || checkUserInput2){
+//     console.log('error')
+//   }else{
+//     console.log(Number(userInputNum1)+Number(userInputNum2))
+//   }
+// }
+// getSumTwoNumber()
+
+//let summa;
+const getSumTwoNumber = function(){
+  // const summa = Number(userInputNum1)+Number(userInputNum2);
+  // return summa;
+  return Number(userInputNum1)+Number(userInputNum2);
 }
-getSumTwoNumber()
+
+//const summa = getSumTwoNumber();
+
+if(checkUserInput1 || checkUserInput2){
+  console.log('error')
+}else{
+  console.log(getSumTwoNumber())
+}
