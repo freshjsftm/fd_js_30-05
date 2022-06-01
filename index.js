2dd885d590c0072628c6c25e9cfb472a3e54f8e0
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-01 15:03:07 +0300

    Add getSumTwoNumber

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-//declaration
-logStrMyFirstFunction();
-function logStrMyFirstFunction(){
-  console.log('myFirstFunction');
-}
-logStrMyFirstFunction();
 
-//expression
-const logStrMySecondFunction = function(){
-  console.log('mySecondFunction');
+const getSumTwoNumber = function(){
+  const userInputNum1 = prompt('Enter first number');
+  const userInputNum2 = prompt('Enter second number');
+
+  const checkUserInput1 = userInputNum1===null || userInputNum1==='' || isNaN(Number(userInputNum1));
+  const checkUserInput2 = userInputNum2===null || userInputNum2==='' || isNaN(Number(userInputNum2));
+
+  if(checkUserInput1 || checkUserInput2){
+    console.log('error')
+  }else{
+    console.log(Number(userInputNum1)+Number(userInputNum2))
+  }
 }
-logStrMySecondFunction();
+getSumTwoNumber()
