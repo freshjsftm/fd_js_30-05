b41ad3d143509b146306e92ed0c4d5ad367b3ddb
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-02 16:05:52 +0300

    Add tasks

--- index.js
+++ index.js
@@ -1,77 +1,81 @@
 /**
- * 
- * @param {any} value1 
- * @param {any} value2 
- * @returns {any | null}
+ * function get perimeter
+ * @param {number} sideSquare
+ * @returns false | number
  */
-const returnMaxOfTwo = function(value1, value2){
-  if(typeof value1 === typeof value2){
-    if(value1>value2){
-      return value1;
-    }
-    return value2;
+const getPerimeterSquare = function (sideSquare = 0) {
+  if (sideSquare <= 0) {
+    return false;
   }
-  return null;
-}
+  return 4 * sideSquare;
+};
 
-console.log(returnMaxOfTwo(true, 5));
-console.log(returnMaxOfTwo(45, 5));
-console.log(returnMaxOfTwo('45', 5));
+//debugger
+const side = 5;
+// console.log(getPerimeterSquare(side));
+// console.log(getPerimeterSquare("5"));
+// console.log(getPerimeterSquare(-5));
+// console.log(getPerimeterSquare());
 
+/**
+ * function check age on adult
+ * @param {number} value
+ * @returns {boolean}
+ */
+const isAdult = function (age, AGE_ADULT = 18) {
+  return age >= AGE_ADULT;
+};
+console.log(isAdult(23));
 
+/**
+ *
+ * @param {number} value1
+ * @param {number} value2
+ * @returns {boolean}
+ */
+const determineMultiplicity = function (value1, value2) {
+  return value1 % value2 === 0;
+};
+console.log(determineMultiplicity(23, 23));
 
+/**
+ *
+ * @param {number}
+ * @returns {boolean}
+ */
+const multiplicityOfNumbers = function (number) {
+  return number > 20 && number % 7 === 0;
+};
+console.log(multiplicityOfNumbers(21));
 
-const compareTypeOfValue = function (value1, value2) {
-  if( typeof value1 === typeof value2) {
-      return compareTwoValue(value1, value2);
+/**
+ *
+ * @param {number} side1
+ * @param {number} side2
+ * @param {number} side3
+ * @returns boolean
+ */
+const determineSideLength = function (side1, side2, side3) {
+  return (
+    side1 < side2 + side3 && side2 < side1 + side3 && side3 < side1 + side2
+  );
+};
+
+console.log(determineSideLength(10, 10, 10));
+console.log(determineSideLength(3, 4, 5));
+console.log(determineSideLength(1, 1, 8));
+
+const getResultNumbers = function (num1, num2) {
+  if (num1 % 2 === 0 && num2 % 2 === 0) {
+    return num1 + num2;
   }
-  return null;
-}
-const compareTwoValue = function (value1, value2) {
-  if(value1 > value2) {
-      return value1;
-  } 
-  return value2;
-}
-
-console.log(compareTypeOfValue('h', 'w'));
-console.log(compareTypeOfValue(15, 'w'));
-
-
-
-/*
-const isEven = function (value) {
-   if(value%2===1) {
-       return false;
-   }
-   return true;
-}
-
-console.log(isEven(4));
-console.log(isEven(3));
-*/
-
-/*
-const takeANumber = function (number) {
-  if (number % 2 === 0) {
-    return true
+  if (num1 % 2 === 1 && num2 % 2 === 1) {
+    return num1 * num2;
   }
-  return false
-}
-console.log(takeANumber(10));
-*/
-
-
-/*
-const isNumberEven = function(number){
-  if(number % 2 === 0){
-    return true
+  if (num1 % 2 === 1) {
+    return num1;
   }
-  return false;
-}
-
-const result = isNumberEven(5);
-console.log(result);
-*/
+  return num2;
+};
 
 
