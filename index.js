846ad97e45e67fa8ab9093752fef56321edd9900
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-06 17:13:13 +0300

    Add practice

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-//debugger
-for(let i=1; i<=MAX_TRY; i++){
-  const PIN = prompt('enter pin');
-  if(PIN === PIN_SECRET){
-    console.log('thanks');
-    break;
+//накопить сумму первых 10 чисел
+/*
+0 = 0+1
+1 = 1+2
+3 = 3+3
+6 = 6+4
+
+1+2+3+4+5 = 15
+*/
+const getSum = function (limit = 10) {
+  let summa = 0;
+  for (let i = 0; i <= limit; i++) {
+    summa += i;
   }
-  if(MAX_TRY === i ){
-    console.log('try end');
-    break;
+  return summa;
+};
+
+//5 ! = 1 * 2 * 3 * 4 * 5 = 120
+
+const getFactorial = function(limit = 5){
+  let factorial = 1n;
+  for(let i=1; i<=limit; i++){
+    factorial *= BigInt(i);
   }
-  console.log('try = ', i );
+  return factorial;
 }
 
 
-
+//2^5 = 1*   2*2*2*2*2 = 32
+//написать функцию, которая возвращает положительную степень числа
+const getPower = function(base=2, pow=5){
+  let res = 1;
+  for(let i=0;i<pow;i++){
+    res *= base;
+  }
+  return res;
+}
