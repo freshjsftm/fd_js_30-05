70f56549f2b8c03be504f02353287a38ca4b8574
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-06 16:05:10 +0300

    Example do/while

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
-const PIN_SECRET = "1111";
-const MAX_TRY = 3;
-//запрашивать у пользователя пин до тех пор, пока он не совпадет с PIN_SECRET
-//ограничьте кол-во вводов попытками
-//выводите сколько попыток осталось после каждого ввода
 
+const count = 15;
 
-//счетчик попыток
-let count = MAX_TRY;
-while (true) {
-  const PIN = prompt("Enter PIN");
-  count--;
-  if (PIN === PIN_SECRET) {
-    console.log("Thank you!");
+while(true){
+  console.log(count)
+  if(count>5){
     break;
   }
-  if(count===0){
-    break;
-  }
-  console.log('try again! rest: ', count);
 }
+
+do {
+  console.log(count)
+} while (count<5);
+
 
 
