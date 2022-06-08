0afd908e9768274e5b0d8239ae35ef01bbdac6cd
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-08 16:32:23 +0300

    Add createFamily

--- index.js
+++ index.js
@@ -25,4 +25,32 @@ function createMultipleTable(){
 }
 
 const multTable = createMultipleTable();
-console.log(multTable);
+//console.log(multTable);
+
+
+/*
++ написать функцию, которая возвращает объект семья
+в начале узнаем сколько членов семьи //2
+именем свойства - ключом - будет имя родственника
+а значением - информация о его родственной принадлежности
+информациюзапрашивать у пользователя через prompt
+*/
+
+// const family = {
+//   'Elon Musk':'brother',
+//   'Elen':'mother'
+// }
+
+
+function createFamily(){
+  const family = {};
+  const countMemberFamily = prompt('how much?');
+  for(let i=0; i<countMemberFamily; i++){
+    const name = prompt('name?');
+    const role = prompt('role?');
+    family[name]= role;
+  }
+  return family;
+}
+
+//console.log(createFamily())
