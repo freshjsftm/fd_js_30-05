25fd91319b83dafc3d7f85132c3bc765808e5c7a
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-08 15:41:56 +0300

    Computed Member Access

--- index.js
+++ index.js
@@ -1,39 +1,16 @@
 
-//создать 3 переменных: две для чисел и одну для знака
-//используя эти переменные составить пример и 
-//вывести его в консоль с ответом
-
-const number1 = 2;
-const number2 = 5;
-const sign = '+';
-const sum = function(n1, n2){return n1+n2;}
-//const strResult = number1+' '+sign+' '+number2+' = '+ sum(number1,number2);
-
-const strResult = `${number1} ${sign} ${number2} = ${sum(number1,number2)}`;
-console.log(strResult);
-
-
-/*
-функция принимает относительный путь 
-style.css |  images/pic.png  |  index.html
-и возвращает валидный url
-
-https://127.0.0.1:5500/index.html
-
-*/
-const getValidURL = function(path='index.html'){
-  return `${PROTOCOL}${LOCAL_SERVER_IP}:${LOCAL_SERVER_PORT}/${path}`;
+const object = {
+  prop1: 'prop value',
+  prop2: 12,
+  'prop 3': true,
+  4:'qwerty',
 }
-//console.log(getValidURL());
-
-
-const user = {
-  name: 'Tom',
-  age: 12,
-  futureAgeInfo: function(n){
-    return `In ${n} years ${this.name} will be ${this.age+n} years old.`;
-  },
-}
-console.log(user.futureAgeInfo(25))
-//добавить метод, который будет принимать число n и возвращать строку
-//user.method(5) // In 5 years Tom will be 17 years old.
+console.log(object['prop 3']);
+console.log(object['prop1']);
+console.log(object.prop1);
+const variable = 4;
+console.log(object[variable]);
+console.log(object[4]);
+debugger
+const variable2 = 'prop2';
+console.log(object[variable2]);
