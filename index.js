22f403220827da1e013bc9d90d4fbac3fa5f05fd
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-08 15:13:14 +0300

    Example template string ``

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
-function Kettle(maxVolume){
-  this.maxVolume = maxVolume;
-  this.volume = 0;
 
-  this.addWater = function(value){
-    if(this.volume+value <= this.maxVolume){
-      this.volume += value;
-    }else{
-      console.log('rest wather:', value - (this.maxVolume-this.volume));
-      this.volume = this.maxVolume;
-    }
-    return this.volume;
-  }
-  
-  this.drainWater = function(value){
-    this.volume -= value;   
-    return this.volume;
-  }
+//создать 3 переменных: две для чисел и одну для знака
+//используя эти переменные составить пример и 
+//вывести его в консоль с ответом
 
+const number1 = 2;
+const number2 = 5;
+const sign = '+';
+const sum = function(n1, n2){return n1+n2;}
+//const strResult = number1+' '+sign+' '+number2+' = '+ sum(number1,number2);
+
+const strResult = `${number1} ${sign} ${number2} = ${sum(number1,number2)}`;
+console.log(strResult);
+
+
+/*
+функция принимает относительный путь 
+style.css |  images/pic.png  |  index.html
+и возвращает валидный url
+
+https://127.0.0.1:5500/index.html
+
+*/
+const getValidURL = function(path='index.html'){
+  return `${PROTOCOL}${LOCAL_SERVER_IP}:${LOCAL_SERVER_PORT}/${path}`;
 }
-
-const pot = new Kettle(200);
-console.log(pot.addWater(15))
-console.log(pot.drainWater(100))
+//console.log(getValidURL());
 
 
-
-
-
+const user = {
+  name: 'Tom',
+  age: 12,
+  futureAgeInfo: function(n){
+    return `In ${n} years ${this.name} will be ${this.age+n} years old.`;
+  },
+}
+console.log(user.futureAgeInfo(25))
+//добавить метод, который будет принимать число n и возвращать строку
+//user.method(5) // In 5 years Tom will be 17 years old.
--- js/constats.js
+++ js/constats.js
@@ -1,2 +1,5 @@
 const MAX_TRY = 3;
 const PIN_SECRET = '1111';
+const PROTOCOL = 'http://';
+const LOCAL_SERVER_IP = '127.0.0.1';
+const LOCAL_SERVER_PORT = '5500';
