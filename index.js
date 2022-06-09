68dc9fe1c38e61368abbd53bb4478d8a09eb4100
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-09 16:09:54 +0300

    Add pop to MyArray

--- index.js
+++ index.js
@@ -4,22 +4,32 @@ function MyArray() {
 }
 //object with logic
 function MyArrayPrototype(){
-   this.push = function (element) {
+  this.push = function (element) {
     this[this.length] = element;
     return ++this.length;
   };
+  this.pop = function(){   
+    if(this.length===0){
+      return;
+    } 
+    const lastElement = this[--this.length];
+    delete this[this.length];
+    return lastElement;
+  }
 }
 MyArray.prototype = new MyArrayPrototype();
 
-
-
-const myArr = new MyArray();
-myArr.push(11);
+const myArr = new MyArray(1,2,3,4,8);
+//myArr.push(11);
 console.log(myArr);
 /*-------------------------*/
-const arr = [];
+const arr = [1,2,3,54,5,8];
+const arr2 = [];
 arr.push(55);
+arr2.push(55);
 console.log(arr);
+
+
 
 
 
