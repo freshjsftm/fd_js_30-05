3604f46a7591cde85f441ad50cc6ee8960ad33b4
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-09 15:42:13 +0300

    Add MyArrayPrototype

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const obj1 = {
-  prop: 123,
-};
-const obj2 = new Object({
-  prop: 1,
-});
-const obj3 = Object({
-  prop: 1,
-});
-
-function MyObject() {
-  if (!new.target) {
-    return new MyObject();
-  }
+//object with data
+function MyArray() {
+  this.length = 0;
 }
+//object with logic
+function MyArrayPrototype(){
+   this.push = function (element) {
+    this[this.length] = element;
+    return ++this.length;
+  };
+}
+MyArray.prototype = new MyArrayPrototype();
 
-const myObj = new MyObject();
-console.log(myObj);
+
+
+const myArr = new MyArray();
+myArr.push(11);
+console.log(myArr);
+/*-------------------------*/
+const arr = [];
+arr.push(55);
+console.log(arr);
+
+
+
