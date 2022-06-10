d7c488528bf865b9a438e6e02c2cdfff8298f9b5
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-10 15:25:19 +0300

    Example prototype

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-function toString(){
+function Animal() {
+  this.go = function () {
+    return (this.name ? this.name : "i'm") + " go";
+  };
+  this.eat = function () {
+    return (this.name ? this.name : "i'm") + " eat";
+  };
+}
+function Man() {
+  this.dansing = function (danse) {
+    return (this.name ? this.name : "i'm") + " dansing " + danse;
+  };
+  this.toString = function () {
     return `${this.name} ${this.sname}`;
-  }
+  };
+}
+Man.prototype = new Animal();
 
-const man = {
-  id:1,
-  name:'Elon',
-  sname:'Musk',
-  toString,
-};
 
-const worked = {
-  role:'builder'
-};
+function User(name, sname) {
+  this.name = name;
+  this.sname = sname;
+}
+User.prototype = new Man();
 
-worked.__proto__ = man;
 
-console.log(worked.toString());
+const user1 = new User('Elon', 'Musk');
+const user2 = new User('Tim', 'Lee');
+
