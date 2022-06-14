e1ad7dff5be7287d666d9f9fb9f5b2df7a13ff44
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-14 14:44:27 +0300

    Example function.bind(this)

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 "use strict";
 
+function logHeader(header){
+  //console.log(this)
+  console.log(`${header} | ${this.title}`)
+}
 const site = {
   title:'green site',
   headers:['Header 1','Header 2','Header 3'],
   showHeaders(){
     console.log(this)
-    this.headers.forEach(function(header){
-      //console.log(this)
-      console.log(`${header} | ${this.title}`)
-    }, this);
+    this.headers.forEach(logHeader.bind(this));
   }
 }
 
@@ -18,4 +19,5 @@ site.showHeaders();
 1 - arrow function
 2 - const self = this;
 3 - argument for function
+4 - function.bind(this)
 */
