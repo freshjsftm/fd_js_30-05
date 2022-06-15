d2e45ce65fbc55c1396d72292428af56baf3c0f4
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-15 14:42:49 +0300

    Example new Boolean()

--- index.js
+++ index.js
@@ -1,50 +1,16 @@
 "use strict";
 
-/*
-4! = 4*3*2*1 = 4 * 3!
-3! = 3*2*1 = 3 * 2!
-2! = 2*1 = 2 * 1!
-1! = 1                //base
-0! = 1                //base
-*/
+const check = new Boolean(false);
 
-const factorial = (num=4) =>{
-  if(num<0){
-    return null;
+//console.log(check.valueOf())
+
+if (check) {
+  console.log("yes");
+  if(check.valueOf()){
+    console.log("true");
+  }else{
+    console.log("false");
   }
-  if(num===1 || num===0){
-    return 1n;
-  }
-  return BigInt(num) * factorial(num-1);
+} else {
+  console.log("no");
 }
-console.log(factorial(3))
-
-
-/*
-2^4 = 2*2*2*2 = 2*2^3
-2^3 = 2*2*2   = 2*2^2
-2^2 = 2*2     = 2*2^1
-2^1 = 2*2^0
-2^0 = 1
-*/
-// const power = (num, pow) =>{
-//   if(pow===0){
-//     return 1;
-//   }
-//   return num *power(num, pow-1);
-// }
-// console.log(power(3, 2)) //9
-
-const power = (num, pow) =>{ 
-  if(pow<0){ 
-    return power(num,pow+1)/num ; 
-    //return 1/num * power(num,pow+1) ; 
-  } 
-  if( pow===0){ 
-    return 1; 
-  }; 
-  if( pow>0){ 
-  return power(num,pow-1)*num 
-}; 
-} 
-console.log(power(2222,22222))
