5982e168fa7732b429072d23d89f98dcd1cfe4fa
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-15 17:05:06 +0300

    Add countSomeLetters

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 "use strict";
 
-//const str = 'to Be or NoT To bE';
 
-//написать, котороя приводит переданную строку
-//к Jaden Case
-//'To be Or Not To Be'
-/*
-1) разбить строку на отдельные слова - массив слов  //split
-2) с каждым словом: //array.map
-    - все слово привести к нижнему регистру и вернуть, //toLowerCase
-      а первую букву - к верхнему                      //toUpperCase
-3) массив слов склеить обратно в строку //array.join   
-*/
 
-const str = "to    Be    or NoT     To    bE";
-const toJadenCase = (str, separator = " ") =>
-  str
-    .split(separator)
-    .filter((word)=> word)
-    .map((word) => word.at(0).toUpperCase() + word.slice(1).toLowerCase())
-    .join(" ");
+//написать функцию, которая принимает строку 
+//и возвращает количество гласных в этой строке. 
+//регистр не учитывать
+// aeiouy
 
-console.log(toJadenCase(str));
+//перевести строку к нижнему регистру
+//разобрать строку на буквы
+//перебирать буквы и если буква гласная - увеличивать счетчик
+
+const str = 'Elon Musk';
+const countSomeLetters = (str, arr = ['a','e','i','o','u','y']) => {
+  return [...str.toLowerCase()]
+    .filter((elem)=>arr.includes(elem))
+    .length;
+}
+console.log(countSomeLetters(str))
+
+
+
+// 'First Lastname' => 'Lastname F.'
