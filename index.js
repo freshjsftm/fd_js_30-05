9b38228e58a5584642be4ac7d26e9819fa7cb266
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-15 15:37:32 +0300

    Example new String()

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 "use strict";
 
-const number = new Number(10.12345);
-console.log(number.toFixed(2));
+const str = new String('Elon');//object
+console.log(str);
 
-const number2 = 45.451245.toFixed(2);//string
-
-const number3 = 123456.12312.toPrecision(3);
-
+const str1 = 'El-on-Mu-sk!';
