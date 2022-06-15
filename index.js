647ed0def072f9ae5d23c6a46661c958aaeba9b1
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-15 16:25:07 +0300

    Example toJadenCase

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 "use strict";
 
-const str = new String('Elon');//object
-console.log(str);
+//const str = 'to Be or NoT To bE';
 
-const str1 = 'El-on-Mu-sk!';
+//написать, котороя приводит переданную строку
+//к Jaden Case
+//'To be Or Not To Be'
+/*
+1) разбить строку на отдельные слова - массив слов  //split
+2) с каждым словом: //array.map
+    - все слово привести к нижнему регистру и вернуть, //toLowerCase
+      а первую букву - к верхнему                      //toUpperCase
+3) массив слов склеить обратно в строку //array.join   
+*/
+
+const str = "to-Be-or-NoT-To-bE";
+const toJadenCase = (str, separator = " ") =>
+  str
+    .split(separator)
+    .map((word) => word.at(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(" ");
+
+console.log(toJadenCase(str,'-'));
