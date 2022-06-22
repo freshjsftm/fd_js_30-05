7b9497d24714e10c2ae08f0168ba4e0ac7a20b70
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-22 15:50:37 +0300

    Add checkBrackets  bad variant

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 "use strict";
+
+const checkBrackets = (str) =>{
+//изначально стек пустой
+  const stack = new Stack(str.length);
+//перебираем символы строки
+  for (const symbol of str) {
+//если символ открывающая скобка - пушим в стек
+    if(symbol === '('){
+      stack.push(symbol);
+    }
+    if(stack.empty){
+      return false;
+    }
+//если символ закрывающая скобка - пикаем стек и 
+//если на врешине стека парная открывающая - 
+//то удаляем ее из стека   
+    if(symbol === ')' && stack.pick()==='('){
+      stack.pop();
+    }
+  }
+//если в конце стек пустой - то последовательность правильная
+  return stack.empty;
+} 
+
+console.log(checkBrackets('()()'))
+console.log(checkBrackets('()'))
+console.log(checkBrackets(')()'))
+console.log(checkBrackets('((()'))
