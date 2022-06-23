40a6d180fca0021b840972f8e522f3f5303b9a2e
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-23 16:50:51 +0300

    Add compare

--- index.js
+++ index.js
@@ -1,46 +1,34 @@
 "use strict";
 
-//деструктуризация - это способ создания переменной
-
-const user = {
-  id:1,
-  privateInfo:{
-    fname:"Elon",
-    sname:"Musk",
-    bday:{
-      day:28,
-      month:6,
-      year:1971,
-    },
-    children:['q1','q2','q3','q4'],
-  },
-  contactInfo:{
-    phone:{
-      work:'123-12-45',
-      mobile:'005-002-003',
-    },
-    adress:{
-      town:'ZP',
-      street:'12Avenu',
-      house:45,
-    },
-    mail:'elon@gmail.com'
-  },
-  profession:'director',
-  date:{
-    day:28,
-    month:6,
-    year:1999,    
+const getMap = (str) => {
+  const strMap = new Map();
+  for (const symbol of str) {
+    if(strMap.has(symbol)){
+      const count = strMap.get(symbol);
+      strMap.set(symbol, count+1);
+      continue;
+    }
+    strMap.set(symbol, 1);
   }
+  return strMap;
+}
+const compare = (str1, str2) => {
+  const str1Map = getMap(str1);
+  const str2Map = getMap(str2);
+  if(str1Map.size !== str2Map.size){
+    return false;
+  }
+  for (const key of str1Map.keys()) {
+    if( str1Map.get(key) !== str2Map.get(key) ){
+        return false;
+    }
+  }
+  return true;
 }
 
-const {privateInfo:{children:[,two]}} = user;
-
-//const prof = user.profession;
-const {profession, contactInfo:{mail}, ...rest} = user;
-
-const nums = [1,2,3,4,5];
-//const one = nums[0];
-//const [one,,,four] = nums;
-const [one,...restNums] = nums;
+console.log(compare('str1', 'str2'))
+console.log(compare('strsrt', 'rstrst'))
+console.log(compare('love', 'evol'))
+console.log(compare('qwerty', 'qwert'))
+console.log(compare('mom', 'omo'))
 
